50b31c52d15bd2b803bb21c1a1604cb7c3d89d6f
Yoan Ivanov <149715053+YokoHouse@users.noreply.github.com>
2024-10-13 15:27:28 +0300

    Create Intro2.cpp
    1.
    Before we move on, let’s write a letter to your future self.
    
    In letter.cpp, let’s add the following:
    
    Goal(s) for yourself.
    Name and date.
    Press Run to mail the letter! 📬
    
    P.S. This letter will be returned when you complete the course.

---
+++ Intro2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() 
+{
+  
+  std::cout << "Dear Self,\n";
+  std::cout << "The goals are to finish this course.\n";
+  std::cout << "Yoan Ivanov, 13.10.24\n";
+  
+  
+  
+  
+  
+}
