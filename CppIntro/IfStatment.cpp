b4ffc7ae6b65a50fae1a0c4cfc2d597f0c466a19
Yoan Ivanov <149715053+YokoHouse@users.noreply.github.com>
2024-10-14 23:43:50 +0300

    IfStatment.cpp

--- CppIntro/IfStatment.cpp
+++ CppIntro/IfStatment.cpp
@@ -1,11 +1,12 @@
-#include <iostream>
-
 int main() {
 
-  int grade = 90;
-  
-  if (grade > 60){
-    std::cout << "Pass";
+  int grade = 59;
+
+  if (grade > 60) {
+
+    std::cout << "Pass\n";
+
+  } else {
+    std::cout << "Fail\n";
   }
   
-}
