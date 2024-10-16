29c74e9780f14627a5fc572edacb69e655cd59d2
Yoan Ivanov <149715053+YokoHouse@users.noreply.github.com>
2024-10-16 16:11:28 +0300

    Update vector.cpp

--- CppIntro/vector.cpp
+++ CppIntro/vector.cpp
@@ -5,8 +5,10 @@ int main() {
    
   std::vector<double> subway_adult = {800, 1200, 1500};
   
-  // Give subway_child some values:
-  
   std::vector<double> subway_child = {400, 600, 750};
+  
+  // What number at index 2 of subway_child?
+  
+  std::cout << subway_child[2] << "\n";
   
 }
