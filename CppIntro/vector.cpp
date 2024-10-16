84202a415471c97c335d5708bd79dc75fbc49bdb
Yoan Ivanov <149715053+YokoHouse@users.noreply.github.com>
2024-10-16 16:10:05 +0300

    Update vector.cpp

--- CppIntro/vector.cpp
+++ CppIntro/vector.cpp
@@ -3,10 +3,10 @@
 
 int main() {
    
-  std::vector<double> subway_adult;
+  std::vector<double> subway_adult = {800, 1200, 1500};
   
-  // Declare another vector here:
-  std::vector<double> subway_child;
-    
+  // Give subway_child some values:
+  
+  std::vector<double> subway_child = {400, 600, 750};
   
 }
