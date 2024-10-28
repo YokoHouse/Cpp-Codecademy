5b4429bf5e4fa0869643ba3193487c11c7dcdac4
Yoan Ivanov <149715053+YokoHouse@users.noreply.github.com>
2024-10-28 16:25:48 +0300

    Create passcode.cpp
    If you run the code, you can print secret_knowledge right in main() without entering the passcode. Yikes!
    
    Only people who enter the correct passcode should have access to that knowledge.
    
    Move secret_knowledge into local scope so that it only prints from the function call when the correct code is entered.
    
    Checkpoint 2 Passed
    2.
    Nice work! Now it’s time to get rid of that error.
    
    Delete the line in main() that prints secret_knowledge directly without doing any math and keep the enter_code(0310);.

---
+++ Funcitons/Functions1/passcode.cpp
@@ -0,0 +1,21 @@
+void enter_code(int passcode) {
+  
+  if (passcode == 0310) {
+    std::string secret_knowledge = "https://content.codecademy.com/courses/regex/onyourexcitingjourneylearningtocodeyouwillfindthis.gif";
+
+    std::cout << secret_knowledge << "\n";
+    
+  } else {
+    
+    std::cout << "Sorry, incorrect!\n";
+    
+  }
+}
+
+int main() {
+  
+
+  
+  enter_code(0310);
+  
+}
